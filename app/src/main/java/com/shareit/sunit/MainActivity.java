7dd9b074d22d458f61ed67745d5f80543e2ffd28
anjs <anjs@ushareit.com>
2020-05-21 21:21:03 +0800

    [SUnit]Remove unused code

--- app/src/main/java/com/shareit/sunit/MainActivity.java
+++ app/src/main/java/com/shareit/sunit/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
-import com.sunit.rate.openapi.SUnitRate;
 import com.ushareit.ads.ad.AdWrapper;
 import com.ushareit.ads.ad.BannerAd;
 import com.ushareit.ads.ad.IAdLoadListener;
